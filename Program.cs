873f11f8753568f3259c5aa35a53e1c83b4c5c84
Anastassia Kuhartsik <nastjonka187@gmail.com>
2022-03-19 10:57:38 +0200

    add my second priject

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UserPIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //приложение просит пользователя ввести PIN-код
+            //если веденный Pin-код "1234", то приложение приветствует пользователя
+            //и спрашивает его имя. А затем приветствует пользователя по имени.
+            //в ином случае, консоль выводит на экран сообщение об ошибке Invalid PIN.
+
+
+            Console.WriteLine("Please enter PIN!");
+            string userName = Console.ReadLine();
+            if (userName == "1234")
+            {
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("What is your name?");
+                string userFirstName = Console.ReadLine();
+                Console.WriteLine(" Hello, " + userFirstName + "!");
+
+            } else
+
+
+            {
+                Console.WriteLine("Invalid PIN!");
+               
+
+            }
+                
+            
+
+                
+            }
+
+        }
+    }
+
+;
